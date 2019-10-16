e70111c1b86f36d254747d713483063b97186dc3
Patryk <patryk.krokosz@smcebi.edu.pl>
2019-10-16 19:56:12 +0200

    Powoli do przodu

--- main.cpp
+++ main.cpp
@@ -2,14 +2,37 @@
 #include <ncurses.h>
 #include "objects.h"
 
+const int ENEMIESNUMBER = 40;
+const int WIDTH = 60;
+const int HEIGHT = 40;
+
 using namespace std;
 
 int main()
 {
 
-    Gorol gorol;
+    Gorol gorole[ENEMIESNUMBER];
 
-    cout<< *gorol.get_pos();
+    int gy = 4;
+    int gx = 4;
+
+    for (int i; i < ENEMIESNUMBER; i++){
+        gorole[i] = Gorol(gy, gx);
+        if (gx > WIDTH-20){
+            gx = 3;
+            gy++;
+        }
+        gx++;
+    }
+
+    initscr();
+    noecho();
+    for(int i; i < ENEMIESNUMBER; i++){
+        mvaddch(gorole[i].get_pos()[0], gorole[i].get_pos()[1], gorole[i].get_face());
+    }
+    refresh();
+    getch();
+    endwin();
 
 	return 0;
 }
--- objects.h
+++ objects.h
@@ -16,7 +16,7 @@ class Player {
     void move_right(){ x++; }
     void move_left(){ x--; }
     int get_lives(){ return lives; }
-    void lose_live() { lives--;}
+    void lose_live() { lives--; }
     int get_pos(){ return x; }
 };
 
@@ -27,9 +27,9 @@ class Gorol {
     char face;
 
   public:
-    Gorol(int ix = 0, int iy = 0, char iface = 'G', bool iisAlive = true){
-        pos[1] = ix;
+    Gorol(int iy = 0, int ix = 0, char iface = 'G', bool iisAlive = true){
         pos[0] = iy;
+        pos[1] = ix;
         face = iface;
         isAlive = iisAlive;
     }
@@ -38,7 +38,8 @@ class Gorol {
     void move_left(){ pos[1]--; }
     void move_down(){ pos[0]--; }
     void die(){ isAlive = false; }
-    int get_pos(){ return pos; }
+    int* get_pos(){ return pos; }
+    char get_face(){ return face; }
 
 
 };
