3e9bdc84bbdb5e49fe4d2ea99cc2ac1d466fc7c6
Patryk <patryk.krokosz@smcebi.edu.pl>
2019-10-16 09:02:23 +0200

    Test

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	initscr();			/* Start curses mode 		  */
-	printw("Hello World !!!");	/* Print Hello World		  */
+	printw("Hello Worl");	/* Print Hello World		  */
 	refresh();			/* Print it on to the real screen */
 	getch();			/* Wait for user input */
 	endwin();			/* End curses mode		  */
